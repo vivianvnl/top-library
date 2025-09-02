82c03c579fdd64ee985f19028d3deede7d143b1a
vivianvnl <224661527+vivianvnl@users.noreply.github.com>
2025-09-02 16:11:21 -0500

    create divs for books added

--- script.js
+++ script.js
@@ -9,11 +9,29 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read, id) {
-    newBook = new Book(title, author, pages, read, id);
-    myLibrary.push(newBook);
+  newBook = new Book(title, author, pages, read, id);
+  myLibrary.push(newBook);
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
-addBookToLibrary('Call me By Your Name', 'Andre Aciman', 260, 'read');
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'no');
+addBookToLibrary('Call me By Your Name', 'Andre Aciman', 256, 'yes');
+addBookToLibrary('My Brilliant Friend', 'Elena Ferrante', 331, 'yes')
 
 console.log(myLibrary);
+
+
+const container = document.getElementById('container');
+
+myLibrary.forEach(item => {
+  const book = document.createElement('div');
+  
+  book.innerHTML = `
+        <h3>${item.title}</h3>
+        <p>Author: ${item.author}</p>
+        <p>Pages: ${item.pages}</p>
+        <p>Read? ${item.read}</p>
+    `;
+
+  book.classList.add = 'books';
+  container.appendChild(book);
+});
