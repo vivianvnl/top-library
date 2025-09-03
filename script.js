400cbfc20ee04387749ab96b02699598783a3803
vivianvnl <224661527+vivianvnl@users.noreply.github.com>
2025-09-03 14:11:41 -0500

    add button to add new books to library using form

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, pages, read, id) {
-  newBook = new Book(title, author, pages, read, id);
+function addBookToLibrary(title, author, pages, read) {
+  newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
 
@@ -20,8 +20,43 @@ addBookToLibrary('My Brilliant Friend', 'Elena Ferrante', 331, 'yes')
 console.log(myLibrary);
 
 
-const container = document.getElementById('container');
+const showButton = document.getElementById('showDialog');
+const dialog = document.querySelector('dialog');
+const addBookBtn = dialog.querySelector("#addBookBtn");
 
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+addBookBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').checked;
+
+  console.log(title, author, pages, read);
+  addBookToLibrary(title, author, pages, read);
+  console.log(myLibrary);
+
+  const container = document.getElementById('container');
+  const book = document.createElement('div');
+  
+  book.innerHTML = `
+        <h2>${title}</h2>
+        <ul>
+          <li><p>Author: ${author}</p></li>
+          <li><p>Pages: ${pages}</p></li>
+          <li><p>Read? ${read}</p></li>
+        </ul>
+    `;
+
+  book.classList.add('books');
+  container.appendChild(book);
+});
+
+/* const container = document.getElementById('container');
 myLibrary.forEach(item => {
   const book = document.createElement('div');
   
@@ -36,4 +71,4 @@ myLibrary.forEach(item => {
 
   book.classList.add('books');
   container.appendChild(book);
-});
+}); */
